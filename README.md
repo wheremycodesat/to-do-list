ca1a6610431df588edc8374b4f730e5ee9e93341
wheremycodesat <samuel.worshop@gmail.com>
2021-07-21 01:15:26 +0200

    final changes, the project is ready to review and test

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-read me file
+Simple to-do list with css BEM convention for styles,
+responsive design and a beautiful final layout
--- js/actions.js
+++ js/actions.js
@@ -1,30 +1,46 @@
-let addItem = (func) => {
-  //func is short for function
-  if (func) {
-    //Variables
-    let list = document.getElementById("item-list");
-    let item_name = document.getElementById("item").value;
-    let listElements = document
-      .getElementById("item-list")
-      .getElementsByTagName("li");
+document.getElementById("submit-button").addEventListener("click", (e) => {
+  //prevent submitting refreshings
+  e.preventDefault();
 
-    let newItem = document.createElement("li");
-    newItem.classList.add("list-element");
-    newItem.id = listElements.length + 1;
-    newItem.innerHTML =
-      item_name +
-      "<span class='remove-todo' onclick='removeItem(" +
-      newItem.id +
-      ")'> &times; </span>";
-    list.appendChild(newItem);
-
-    document.getElementById("item").value = "";
-  } else {
-    return false;
+  //control to eliminate empty list's message
+  if (document.getElementById("list").children[0].id === "temporal-text") {
+    let temp = document.getElementById("temporal-text");
+    temp.remove();
   }
-};
+
+  //Variables
+  let list = document.getElementById("list");
+  let todo_name = document.getElementById("todo-name").value;
+  let number_of_elements = document.getElementById("list").childElementCount;
+
+  //New todo
+  let new_todo = document.createElement("p");
+  new_todo.classList.add("todo");
+  new_todo.id = number_of_elements + 1;
+  new_todo.innerHTML =
+    todo_name +
+    "<span class='todo--remove' onclick='removeItem(" +
+    new_todo.id +
+    ")'> &times; </span>";
+
+  list.appendChild(new_todo);
+
+  document.getElementById("todo-name").value = "";
+});
 
 let removeItem = (itemId) => {
   let item = document.getElementById(itemId);
+
+  //Insert empty list's message again
+  if (document.getElementById("list").childElementCount === 1) {
+    item.remove();
+    let p = document.createElement("p");
+    p.classList.add("temporal-text");
+    p.id = "temporal-text";
+    p.innerHTML = "The list is empty";
+    let l = document.getElementById("list");
+    l.appendChild(p);
+  }
+
   item.remove();
 };
