b568246756f3e99942f771ac8dc5deeb7202adf1
wheremycodesat <samuel.worshop@gmail.com>
2021-11-12 15:59:18 +0100

    dark mode improvements

--- js/dark_toggle.js
+++ js/dark_toggle.js
@@ -1,10 +1,26 @@
-let toggle = document.getElementById("switch");
-let html = document.getElementsByTagName("html")[0];
-
-toggle.addEventListener("change", () => {
-  if (toggle.checked) {
-    html.setAttribute("theme", "dark-theme");
-  } else {
-    html.setAttribute("theme", "light-theme");
-  }
+document.addEventListener("DOMContentLoaded", function () {
+  darkMode();
 });
+
+function darkMode() {
+  const toggle = document.getElementById("switch");
+  const darkModePreference = window.matchMedia("(prefers-color-scheme: dark)");
+
+  darkModePreference.addEventListener("change", function () {
+    if (darkModePreference.matches) {
+      document.body.classList.add("dark-theme");
+      toggle.setAttribute("checked", true);
+    } else {
+      document.body.classList.remove("dark-theme");
+      toggle.setAttribute("checked", false);
+    }
+  });
+
+  toggle.addEventListener("change", () => {
+    if (toggle.checked) {
+      document.body.classList.add("dark-theme");
+    } else {
+      document.body.classList.remove("dark-theme");
+    }
+  });
+}
